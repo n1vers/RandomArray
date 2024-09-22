4da9c5f96626fc3d8109166784601561507261dd
n1vers <nikita.andronov@ivkhk.ee>
2024-09-22 20:36:45 +0300

    1.0

---
+++ src/main/java/ee/ivkhkdev/App.java
@@ -0,0 +1,34 @@
+package ee.ivkhkdev;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class App {
+    public void run(){
+        Random random = new Random();
+        int[] number = new int[20];
+
+        for (int i = 0; i < number.length; i++) {
+            number[i] = random.nextInt(50+1)*2;
+        }
+        System.out.println("Array"+ Arrays.toString(number));
+
+        int min = Arrays.stream(number).min().getAsInt();
+        int max = Arrays.stream(number).max().getAsInt();
+
+        System.out.println("Минимальный элемент: " + min);
+        System.out.println("Максимальный элемент: " + max);
+
+        int sum=0;
+        int count=0;
+        for (int num: number) {
+            if (num != min && num != max) {
+                sum += num;
+                count++;
+            }
+        }
+
+        double average = (double) sum / count;
+        System.out.printf("Среднее арифметическое без учета минимального и максимального: %.2f",average);
+    }
+}
